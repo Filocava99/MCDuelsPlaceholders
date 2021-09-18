7b4ae1f69986a3af394104b5fb78de9c2991ebdb
Tigierrei <tigierrei22@gmail.com>
2021-09-18 11:24:26 +0200

    Fixed placeholder identifier

--- src/main/kotlin/it/ancientrealms/mcduelsplaceholders/Expansion.kt
+++ src/main/kotlin/it/ancientrealms/mcduelsplaceholders/Expansion.kt
@@ -9,7 +9,7 @@ class Expansion : PlaceholderExpansion() {
 
     override fun getRequiredPlugin() = "McDuels"
 
-    override fun getIdentifier(): String = McDuelsPlaceholders.INSTANCE.description.name
+    override fun getIdentifier(): String = "mcduelsph"
 
     override fun getAuthor(): String = McDuelsPlaceholders.INSTANCE.description.authors.joinToString(", ")
 
@@ -17,14 +17,14 @@ class Expansion : PlaceholderExpansion() {
 
     override fun canRegister(): Boolean = Bukkit.getPluginManager().getPlugin("McDuels") != null
 
+    override fun persist(): Boolean = true
+
     override fun onRequest(player: OfflinePlayer?, params: String): String {
-        if(params.equals("mcduels_top_elo_1_name")){
-            McDuels.datamanager.getTopPlayers(1, "Elo")[0].name
-        }else if(params.equals("mcduels_top_elo_2_name")){
-            McDuels.datamanager.getTopPlayers(2, "Elo")[1].name
-        }else if(params.equals("mcduels_top_elo_3_name")){
-                McDuels.datamanager.getTopPlayers(3, "Elo")[2].name
+        return when (params) {
+            "top_elo_1_name" -> McDuels.datamanager.getTopPlayers(1, "Elo")[0].name
+            "top_elo_2_name" -> McDuels.datamanager.getTopPlayers(2, "Elo")[1].name
+            "top_elo_3_name" -> McDuels.datamanager.getTopPlayers(3, "Elo")[2].name
+            else -> "Not found"
         }
-        return ""
     }
 }
